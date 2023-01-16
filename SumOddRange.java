23e6dbf71ec0ff72a6fd753a99f0371766290012
Glitch-1010 <56766914+Glitch-1010@users.noreply.github.com>
2023-01-16 16:35:51 +0800

    Add files via upload

---
+++ SumOddRange.java
@@ -0,0 +1,22 @@
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+           if ((number > 0) && (number % 2 != 0)) {
+               return true;
+           }
+           return false;
+       }
+   
+       public static int sumOdd(int start, int end) {
+           int sumOfTheOddNumbers = 0;
+           if (start > 0 && end > 0 && end >= start) {
+               for (int i = start; i <= end; i++) {
+                   if (isOdd(i)) {
+                       sumOfTheOddNumbers = sumOfTheOddNumbers + i;
+                   }
+               }
+               return sumOfTheOddNumbers;
+           } else {
+               return -1;
+           }
+       }
+   }
